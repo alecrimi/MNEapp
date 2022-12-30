0b427f7461e2949dd9dad4333650c8f03c426976
Alessandro Crimi <alessandro.crimi@iit.it>
2022-12-31 00:46:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,15 @@
 import streamlit as st
 import dipy as dipy
 import dipy.io as dipy_io
+from dipy.io.streamline import load_tractogram, save_tractogram
 
 # Display a file upload widget
 trk_file = st.file_uploader("Upload a TRK file")
-st.write(dipy.__version__)
+ 
 
 # Load the TRK file using DiPy
 if trk_file is not None:
-    trk, hdr = dipy.io.streamline.load_trk(trk_file)
+    cc_trk = load_tractogram(trk_file, 'same') #trk, hdr = dipy.io.streamline.load_trk(trk_file)
     st.write("TRK file successfully loaded!")
 
     # Display the TRK file using the Streamlit 3D scatterplot widget
