78909097c6492aea0d98a17adef6ed5d4107ed24
Alessandro Crimi <alessandro.crimi@iit.it>
2022-12-31 00:34:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import dipy as dipy
+import dipy.io as dipy_io
 
 # Display a file upload widget
 trk_file = st.file_uploader("Upload a TRK file")
 
 # Load the TRK file using DiPy
 if trk_file is not None:
-    trk, hdr = dipy.io.read_trk(trk_file)
+    trk, hdr = dipy_io.read_trk(trk_file)
     st.write("TRK file successfully loaded!")
 
     # Display the TRK file using the Streamlit 3D scatterplot widget
