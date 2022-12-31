cacb673877e0631cce257e521f83cb8f857a09c1
Alessandro Crimi <alessandro.crimi@iit.it>
2022-12-31 01:19:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import dipy as dipy
 import os
 import dipy.io as dipy_io
 from dipy.io.streamline import load_tractogram, save_tractogram
-
+'''
 # Display a file upload widget
 upload = st.file_uploader("Upload a TRK file", type=['trk'])
 
@@ -21,11 +21,13 @@ def get_uploaded_image():
         return file_path # fixed indentation
 
 user_trk = get_uploaded_image()
-
+'''
 # Load the TRK file using DiPy
+user_trk= 'https://github.com/alecrimi/MNEapp/blob/main/CST.trk'
+
 if upload is not None:
     trk = load_tractogram(user_trk, 'same') #trk, hdr = dipy.io.streamline.load_trk(trk_file)
-    st.write("TRK file successfully loaded!")
+    #st.write("TRK file successfully loaded!")
 
     # Display the TRK file using the Streamlit 3D scatterplot widget
     st.pyplot(dipy.viz.window.show(trk, title='TRK file', size=(600, 600)))
