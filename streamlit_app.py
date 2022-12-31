9d271662306ede679fa7389b3d7b7c792bf14a92
Alessandro Crimi <alessandro.crimi@iit.it>
2022-12-31 01:00:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,8 @@ st.write(trk_file.name)
 def get_uploaded_file():
     return trk_file 
 
-trk_file = get_uploaded_file()
+uploadedfile = get_uploaded_file()
+st.write(uploadedfile)
 # Load the TRK file using DiPy
 if trk_file is not None:
     cc_trk = load_tractogram(trk_file.name, 'same') #trk, hdr = dipy.io.streamline.load_trk(trk_file)
